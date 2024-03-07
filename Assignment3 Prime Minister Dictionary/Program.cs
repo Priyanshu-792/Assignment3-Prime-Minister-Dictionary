300f72abc8ca5db4017e67f831e15916f0af7a3a
Priyanshu Sharma <priyanshusharma72002@gmail.com>
2024-03-08 00:37:06 +0530

    Add project files.

---
+++ Assignment3 Prime Minister Dictionary/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+class IndiaLeaders
+{
+    static void Main(string[] args)
+    { 
+        //initial dictionary of prime ministers
+        var primeMinisters = new Dictionary<string, int>()
+        {
+            {"Atal Bihari Vajpayee", 1998},
+            {"Narendra Modi", 2014},
+            {"Manmohan Singh", 2004}
+        };
+
+        string pm2004 = GetPMOfYear(primeMinisters, 2004);
+        Console.WriteLine($"Prime Minister of 2004: {pm2004}");
+
+        primeMinisters.Add("Narendra Damodardas Modi", 2024);
+
+        var sortedPrimeMinisters = SortPrimeMinistersByYear(primeMinisters);
+
+        // Display the sorted prime ministers
+        Console.WriteLine("\nPrime Ministers Year Wise:");
+        foreach (var pm in sortedPrimeMinisters)
+        {
+            Console.WriteLine($"{pm.Key}: {pm.Value}");
+        }
+    }
+    //finding the prime minister of a specific year
+    static string GetPMOfYear(Dictionary<string, int> primeMinisters, int year)
+    {
+        foreach (var pm in primeMinisters)
+        {
+            if (pm.Value == year)
+                return pm.Key;
+        }
+        return "Prime Minister not found";
+    }
+
+    // Method to sort the prime ministers by year
+    static Dictionary<string, int> SortPrimeMinistersByYear(Dictionary<string, int> primeMinisters)
+    {
+        var sortedPMs = new Dictionary<string, int>();
+
+        var years = new List<int>(primeMinisters.Values);
+        years.Sort();
+
+        foreach (var year in years)
+        {
+            foreach (var pm in primeMinisters)
+            {
+                if (pm.Value == year)
+                {
+                    sortedPMs.Add(pm.Key, pm.Value);
+                    break;
+                }
+            }
+        }
+
+        return sortedPMs;
+    }
+}
